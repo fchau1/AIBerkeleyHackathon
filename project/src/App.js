2ae149c0290b6f76c00c0c0165af8b3ad1cd759b
Fiorina Chau <fchau@nevada.unr.edu>
2024-06-23 02:19:31 -0700

    Connected Calibration to UI + other changes

--- project/src/App.js
+++ project/src/App.js
@@ -1,40 +1,38 @@
-import './App.css';
-import React, { useState } from 'react';
-import PDFReader from './components/PDFReader';
-import PDFPanel from './components/PDFPanel';
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import PDFPanel from "./components/PDFPanel";
 import pdfToText from "react-pdftotext";
 
 function App() {
-  const [extractedText, setExtractedText] = useState("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has");
+  const [submitPDF, setSubmitPDF] = useState(false);
+  const [extractedText, setExtractedText] = useState(
+    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has"
+  );
 
-  const handleFileChange = (event) => {
+
+const handleFileChange = (event) => {
     const file = event.target.files[0];
     pdfToText(file)
-    .then((text) => setExtractedText(text))
+        .then((text) => setExtractedText(text))
+        .catch((error) => console.error("Failed to extract text from pdf"));
+    setSubmitPDF(true);
+    };
 
-    .catch((error) => console.error("Failed to extract text from pdf"));
-  };
+    useEffect(()=>{
 
+    },[submitPDF])
 
   return (
-
-<div className="App h-screen">
-      <header className="App-header h-full">
-        {/* <div className="flex flex-col w-full h-full"> */}
-          {/* <input type="file" accept="application/pdf" onChange={handleFileChange} className="file-input file-input-xs file-input-bordered file-input-primary w-full grid card bg-base-300 rounded-box place-items-center" /> */}
-          
-          <div className=
-          "flex flex-col h-full w-full lg:flex-row py-4"
-          >
-            <PDFPanel extractedText={extractedText}/>
-            <div className="divider lg:divider-horizontal"></div>
-            <div className="grid h-full py-4 w-3/10 card bg-base-300 rounded-box place-items-center"></div>
-          </div>
-        {/* </div>  */}
-      </header>
+    <div style={{ display: "flex", flexDirection: "row", height: "95vh", width: "100%",  boxSizing: "border-box" }}>
+      {submitPDF ? <>
+      <PDFPanel extractedText={extractedText} />
+        <div style={{ height: "1px", backgroundColor: "#d1d5db", margin: "1rem 0" }}></div> {/* equivalent to divider */}
+        <div style={{ display: "grid", height: "100%", padding: "1rem", width: "25%", backgroundColor: "#2d3748", borderRadius: "0.5rem", placeItems: "center" }}></div>
+        </> :
+        <input type="file" accept="application/pdf" onChange={handleFileChange} className="file-input file-input-xs file-input-bordered file-input-primary w-full grid card bg-base-300 rounded-box place-items-center" />
+      }
     </div>
-
   );
-};
+}
 
 export default App;
--- project/src/components/PDFPanel.tsx
+++ project/src/components/PDFPanel.tsx
@@ -1,18 +1,23 @@
 // src/components/PDFReader.js
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useState } from "react";
 import "@react-pdf-viewer/core/lib/styles/index.css";
 import "@react-pdf-viewer/highlight/lib/styles/index.css";
 import { TileType } from "../Types";
 import Tile from "./Tile";
+import ArrowBackIosNewIcon from "@mui/icons-material/ArrowBackIosNew";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
+import IconButton from "@mui/material/IconButton";
+import GazeTracker from "../calibrations/GazeTracker";
 
 interface PDFPanelProps {
   extractedText: string;
 }
 
 const PDFPanel: React.FC<PDFPanelProps> = ({ extractedText }) => {
+  const [isCalibrated, setIsCalibrated] = useState(false);
   const words = extractedText?.split(" ");
 
-  const containerRef = useRef<HTMLDivElement>(null);
+  // const containerRef = useRef<HTMLDivElement>(null);
   const [windowHeight, setWindowHeight] = useState<number>(window.innerHeight);
 
   const tiles: TileType[] = words.map(
@@ -39,70 +44,52 @@ const PDFPanel: React.FC<PDFPanelProps> = ({ extractedText }) => {
     };
   }, []);
 
-  //Calculate width of panel
-
-  //Calculate width of a letter
-
-  //calculate height of tiles
-
-  //break extractedText into an array/hashmap of words
-
-  //for each word in the array/hashmap, calculate the width of the word
-
-  //if the word is less than the remaining space left in the line, add it to the line
-
-  //else: start a new line
-
   return (
-    <div
-      className="grid h-full w-7/10 card bg-base-300 rounded-box place-items-center h-90 text-6xl w-15 border-2 border-white text-left leading-snug align-middle gap-2"
-      style={{
-        display: "flex",
-        flexDirection: "row",
-        alignItems: "center",
-        gap: "0.5rem",
-        flexWrap: "wrap",
-        height: `${windowHeight}px`,
-      }}
-    >
-      {tiles.map((tile, index) => (
-        <Tile key={index} tile={tile} />
-      ))}
-      <div className="flex justify-center items-center space-x-4 w-full">
-        <button className="btn btn-circle btn-outline">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="size-6"
+    <>
+      {isCalibrated ? (
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "center",
+            gap: "0.5rem",
+            flexWrap: "wrap",
+            height: `100%`,
+            width: "75%", // equivalent to w-8/10
+            backgroundColor: "#2d3748", // equivalent to bg-base-300
+            borderRadius: "0.5rem", // equivalent to rounded-box
+            padding: "1rem", // equivalent to place-items-center
+            fontSize: "3.75rem", // equivalent to text-6xl
+            border: "2px solid white", // equivalent to border-2 border-white
+            textAlign: "left", // equivalent to text-left
+            lineHeight: "1.375", // equivalent to leading-snug
+            verticalAlign: "middle", // equivalent to align-middle
+          }}
+        >
+          {tiles.map((tile, index) => (
+            <Tile key={index} tile={tile} />
+          ))}
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              gap: "1rem",
+              width: "100%",
+            }}
           >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M15.75 19.5 8.25 12l7.5-7.5"
-            />
-          </svg>
-        </button>
-        <button className="btn btn-circle btn-outline">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="size-6"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="m8.25 4.5 7.5 7.5-7.5 7.5"
-            />
-          </svg>
-        </button>
-      </div>
-    </div>
+            <IconButton aria-label="delete">
+              <ArrowBackIosNewIcon />
+            </IconButton>
+            <IconButton aria-label="delete">
+              <ArrowForwardIosIcon />
+            </IconButton>
+          </div>
+        </div>
+      ) : (
+        <GazeTracker windowHeight={windowHeight}/>
+      )}
+    </>
   );
 };
 
